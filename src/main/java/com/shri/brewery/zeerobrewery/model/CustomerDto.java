1dc961e19f5fe5b285cdc44f6b2180715b813c04
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-05 04:59:13 +0530

    Required files added. Closes #5, #6, #7.

---
+++ src/main/java/com/shri/brewery/zeerobrewery/model/CustomerDto.java
@@ -0,0 +1,8 @@
+/*******************************************************************************
+ * Created by  ZeeroIQ on $today.date.
+ ******************************************************************************/
+
+package com.shri.brewery.zeerobrewery.model;
+
+public class CustomerDto {
+}
---
+++ src/main/java/com/shri/brewery/zeerobrewery/service/CustomerServiceImpl.java
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * Created by  ZeeroIQ on $today.date.
+ ******************************************************************************/
+
+package com.shri.brewery.zeerobrewery.service;
+
+import com.shri.brewery.zeerobrewery.model.CustomerDto;
+
+import java.util.UUID;
+
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public CustomerDto getCustomerById(UUID customerId) {
+        return null;
+    }
+}
