702ced5487e8ca717f449be15cf9d89ffbf39bed
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-07 01:32:28 +0530

    Created branch v2 for api-v2. closes #10

---
+++ src/main/java/com/shri/brewery/zeerobrewery/controller/v2/CustomerControllerV2.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Created by  ZeeroIQ.
+ ******************************************************************************/
+
+package com.shri.brewery.zeerobrewery.controller.v2;
+
+
+import com.shri.brewery.zeerobrewery.model.CustomerDto;
+import com.shri.brewery.zeerobrewery.service.contracts.CustomerService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v2/customer")
+public class CustomerControllerV2 {
+
+    private CustomerService customerService;
+
+    public CustomerControllerV2(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<CustomerDto> getCustomer(@PathVariable("customerId")UUID customerId) {
+
+        return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody CustomerDto customerDto) {
+        CustomerDto saveCustomer = customerService.saveCustomer(customerDto);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/customer/" + saveCustomer.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{customerId}")
+    public ResponseEntity handleUpdate(@PathVariable("customerId") UUID beerId, @RequestBody CustomerDto customerDto) {
+        customerService.updateCustomer(beerId, customerDto);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{customerId}")
+    public ResponseEntity handleDelete(@PathVariable("customerId") UUID customerId) {
+        customerService.deleteCustomer(customerId);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/com/shri/brewery/zeerobrewery/model/v2/BeerDto.java
@@ -0,0 +1,23 @@
+package com.shri.brewery.zeerobrewery.model.v2;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerDto implements Serializable {
+
+    private UUID id;
+    private String beerName;
+    private BeerStyleDto beerStyle;
+    private Long utc;
+
+}
