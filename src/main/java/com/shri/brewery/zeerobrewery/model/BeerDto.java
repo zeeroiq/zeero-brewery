98858176b5ff91c00b3576a94b358e8307f93fa1
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-09 02:14:48 +0530

    Closes #18

--- src/main/java/com/shri/brewery/zeerobrewery/model/BeerDto.java
+++ src/main/java/com/shri/brewery/zeerobrewery/model/BeerDto.java
@@ -6,7 +6,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Data
@@ -15,9 +21,23 @@ import java.util.UUID;
 @AllArgsConstructor
 public class BeerDto implements Serializable {
 
+    @Null
     private UUID id;
+    @Null
+    private Integer version;
+    @Null
+    private OffsetDateTime createdOn;
+    @Null
+    private OffsetDateTime lastModifiedOn;
+    @NotBlank
     private String beerName;
+    @NotNull
     private String beerStyle;
+    @Positive
+    @NotNull
     private Long utc;
-
+    @Positive
+    @NotNull
+    private BigDecimal price;
+    private Integer quantityOnHand;
 }
